9bee4716a699d861c5c6159f73f2413cbb302f6d
EricWRogers <ericwilliamrogers@gmail.com>
2025-02-04 12:38:28 -0600

    Vector Math

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,33 @@
 #endif
 
 #include <iostream>
+#include <vector>
 
-int main(int argc, char* argv[])
+#include "fogpi/Math.hpp"
+
+int main(int argc, char *argv[])
 {
-    std::cout << "Hello, World!\n";
+    Vector2D v(5.0f);
+    Vector2D v1(2.0f, 10.0f);
+
+    v = v + v1;
+    v -= v1;
+
+    std::cout << v.x << " " << v.y << std::endl;
+
+    int r = rand() % 5;
+
+    std::vector<Vector2D> points = {};
+
+    for (int i = 0; i < 1000; i++)
+    {
+        // Add
+        points.push_back(Vector2D(5.0f));
+    }
+
+    Print(points[0]);
+
+    std::cout << "Size: " << points.size() << std::endl;
 
     return 0;
 }
