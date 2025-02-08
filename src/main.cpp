3fd60fdd39ca22e62f15b2a26bed7d356e44e6a6
EricWRogers <ericwilliamrogers@gmail.com>
2025-02-07 22:19:42 -0600

    messages

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@
 #include <vector>
 #include "fogpi/Math.hpp"
 #include <time.h>
+#include <stdio.h>
+#include <string.h>
 
 
 
@@ -18,79 +20,13 @@ int Add(int &_a, int &_b)
 
 int main(int argc, char* argv[])
 {
-    int a = 2;
-    int b = 3;
+    char e = 'a';
 
-    std::cout << "Hello, World!" << Add(a,b) << "\n";
+    const char* message = "Hello, World!";
 
-    std::cout << a << std::endl;
+    int a[] = {1,2,3,4};
 
-    Vector2D v(5.0f,5.0f);
-    Vector2D v1(2.0f, 10.0f);
-
-    v=v+v1;
-    v-=v1;
-
-    std::cout << v.x << " " << v.y << std::endl;
-
-    std::vector<Vector2D> points = {};
-
-    srand(time(NULL));
-
-    for (int i = 0; i < 1000; i++){
-        int _tempX = (rand()%1001) - 500;
-        int _tempY = (rand()%1001) - 500;
-        if (i == 0)
-            Print(Vector2D(_tempX, _tempY));
-        points.push_back(Vector2D(_tempX, _tempY));
-    }
-    int max = 0;
-    int min = 0;
-    std::vector<int> quadrents = {0,0,0,0};
-    
-    for (int i =0; i <1000; i++){
-        //Checkign for farthest point positive
-        if (points[i].x +points[i].y > points[max].x + points[max].y){
-            max = i;
-        }
-        //Checking for farthest negative number
-        if (points[i].x +points[i].y < points[min].x + points[min].y){
-            min = i;
-        }
-
-        if (points[i].x < 0){
-            if (points[i].y <0)
-            {
-                quadrents[2] +=1;
-            }
-            else{
-                quadrents[1] +=1;
-            }
-        }
-        else{
-
-            if (points[i].y <0)
-            {
-                quadrents[4] +=1;
-            }
-            else{
-                quadrents[0] +=1;
-            }
-        }
-
-
-    }
-
-    
-
-    Print(points[max]);
-    Print(points[min]);
-    std::cout << "Quadrents 1: " << quadrents[0] << std::endl
-             << "Quadrents 2: " << quadrents[1] << std::endl 
-             <<"Quadrents 3: " << quadrents[2] << std::endl 
-             << "Quadrents 4: " << quadrents[0] << std::endl;
-
-    std::cout << "Size: " << time(NULL) << std::endl;
+    printf("%s %c %i %i %i\n", message, e, strlen(message), *(message+1), *(message+3));
 
     return 0;
 }
