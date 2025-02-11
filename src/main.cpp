1a2779c9041c93be54fb40c934420d5cec1fbb37
EricWRogers <ericwilliamrogers@gmail.com>
2025-02-11 13:53:03 -0600

    Die Stats

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@
 #include <stdio.h>
 #include <string.h>
 
-
+#include "Dice.hpp"
 
 int Add(int &_a, int &_b)
 {
@@ -20,6 +20,8 @@ int Add(int &_a, int &_b)
 
 int main(int argc, char* argv[])
 {
+    srand(time(NULL));
+
     char e = 'a';
 
     const char* message = "Hello, World!";
@@ -27,6 +29,23 @@ int main(int argc, char* argv[])
     int a[] = {1,2,3,4};
 
     printf("%s\n", message);
+
+    using namespace std;
+
+    vector<Die> dice = {};
+
+    Die d6 = {};
+    d6.sides = 6;
+
+    Die die2 = { .sides = 12 };
+
+    dice.push_back(d6);
+    dice.push_back(die2);
+    dice.push_back(Die{.sides = 20});
+
+    RollStats stats = RollDice(dice);
+
+    PrintRollStats(stats);
   
     return 0;
 }
